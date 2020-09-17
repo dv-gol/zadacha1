8cd8c21af84898075fb0fef8d3572195f30fa97c
Dasha <71096430+dv-gol@users.noreply.github.com>
2020-09-17 21:55:05 +0500

    Add files via upload

---
+++ ConsoleApplication4/ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int x, y;
+    cout << "Vvedite chislo 1:";
+    cin >> x;
+    cout << "Vvedite chislo 2:";
+    cin >> y;
+
+    cout << "x + y = " << x + y << endl;
+
+    return 0;
+}
